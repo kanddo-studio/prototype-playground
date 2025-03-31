43059943c5aa6f2fc9d65813d66fdf2c3fcb788d
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-03-30 22:48:03 -0300

    fix: lint

--- src/__tests__/__mocks__/phaser.ts
+++ src/__tests__/__mocks__/phaser.ts
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 export class Sprite {
   public x: number;
@@ -19,25 +19,32 @@ export class Sprite {
 export class Scene {
   public add: { sprite: jest.Mock };
   public input: { on: jest.Mock };
-  public cameras: { main: { scrollX: number; scrollY: number; stopFollow: jest.Mock; startFollow: jest.Mock } };
+  public cameras: {
+    main: {
+      scrollX: number;
+      scrollY: number;
+      stopFollow: jest.Mock;
+      startFollow: jest.Mock;
+    };
+  };
 
   constructor() {
     this.add = { sprite: jest.fn() };
     this.input = { on: jest.fn() };
     this.cameras = {
-        main: {
-          scrollX: 0,
-          scrollY: 0,
-          stopFollow: jest.fn(),
-          startFollow: jest.fn(),
-        }
-      };
+      main: {
+        scrollX: 0,
+        scrollY: 0,
+        stopFollow: jest.fn(),
+        startFollow: jest.fn(),
+      },
+    };
   }
 }
 
 const PhaserMock = {
   GameObjects: { Sprite },
-  Scene
+  Scene,
 };
 
 export default PhaserMock;
--- src/__tests__/core/components/SpriteComponent.test.ts
+++ src/__tests__/core/components/SpriteComponent.test.ts
@@ -1,37 +1,42 @@
-import Phaser from 'phaser';
-import { SpriteComponent } from '../../../core/components/SpriteComponent';
+import Phaser from "phaser";
+import { SpriteComponent } from "../../../core/components/SpriteComponent";
 
-describe('SpriteComponent', () => {
+describe("SpriteComponent", () => {
   let mockScene: Phaser.Scene;
   let mockSprite: Phaser.GameObjects.Sprite;
 
   beforeEach(() => {
     mockScene = new Phaser.Scene() as Phaser.Scene;
-    mockSprite = new Phaser.GameObjects.Sprite(mockScene, 10, 20, 'testTexture');
+    mockSprite = new Phaser.GameObjects.Sprite(
+      mockScene,
+      10,
+      20,
+      "testTexture",
+    );
   });
 
-  it('should initialize with the correct sprite', () => {
+  it("should initialize with the correct sprite", () => {
     const component = new SpriteComponent(mockSprite);
 
     expect(component.sprite).toBe(mockSprite);
   });
 
-  it('should have correct initial properties', () => {
+  it("should have correct initial properties", () => {
     const component = new SpriteComponent(mockSprite);
 
     expect(component.sprite.x).toBe(10);
     expect(component.sprite.y).toBe(20);
-    expect(component.sprite.texture.key).toBe('testTexture');
+    expect(component.sprite.texture.key).toBe("testTexture");
   });
 
-  it('should be able to set new texture', () => {
+  it("should be able to set new texture", () => {
     const component = new SpriteComponent(mockSprite);
-    component.sprite.setTexture('newTexture');
+    component.sprite.setTexture("newTexture");
 
-    expect(component.sprite.setTexture).toHaveBeenCalledWith('newTexture');
+    expect(component.sprite.setTexture).toHaveBeenCalledWith("newTexture");
   });
 
-  it('should be able to set position', () => {
+  it("should be able to set position", () => {
     const component = new SpriteComponent(mockSprite);
     component.sprite.setPosition(30, 40);
 
